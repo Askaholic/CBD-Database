1dbd76cca6e9878de23b762a4c8f2f5fc6b7e246
Askaholic <Askaholic907@gmail.com>
2018-01-24 18:25:46 -0900

    Setup test file to verify wordpress plugin installation

---
+++ src/danceparty.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @package DanceParty
+ */
+/*
+Plugin Name: Dance Party
+Description: This plugin adds access for Contra Borealis member and event management
+Author: DanceParty
+Version: 0.0.0
+ */
+?>

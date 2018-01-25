346c71278cbb40db16b253751bab7888afa60310
Askaholic <Askaholic907@gmail.com>
2018-01-24 19:27:10 -0900

    Added some basic structure and security precautions

--- src/danceparty.php
+++ src/danceparty.php
@@ -9,4 +9,20 @@ Description: This plugin adds access for Contra Borealis member and event manage
 Author: DanceParty
 Version: 0.0.0
  */
+
+if ( !function_exists( 'add_action' ) ) {
+  echo "You can't access plugins directly. To use this plugin, simply activate it through the admin menu.";
+  exit;
+}
+
+add_action('admin_menu', 'danceparty_setup_menu');
+
+function danceparty_setup_menu() {
+  add_menu_page('Dance Party Settings', 'Dance Party', 'manage_options', 'danceparty-plugin', 'test_init');
+}
+
+function test_init() {
+  include(plugin_dir_path( __FILE__ ) . 'views/settings.php');
+}
+
 ?>

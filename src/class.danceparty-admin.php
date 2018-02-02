f1a7b01ac1b963c062a82f98c4491da141f99429
Askaholic <Askaholic907@gmail.com>
2018-02-01 18:52:14 -0900

    Added a single option through the wordpress options API. OH MY GOD it's the worst thing I've ever used!

--- src/class.danceparty-admin.php
+++ src/class.danceparty-admin.php
@@ -18,15 +18,46 @@ class DancePartyAdmin
 
     static function init_hooks() {
         add_action( 'admin_menu', array( 'DancePartyAdmin', 'setup_menu' ) );
+        add_action( 'admin_init', array( 'DancePartyAdmin', 'register_settings' ) );
 
         self::$init_done = true;
     }
 
     static function setup_menu() {
-        add_menu_page( DanceParty::NAME . ' Settings',
-                       DanceParty::NAME, 'manage_options',
-                       'danceparty-plugin',
-                       array('DancePartyAdmin', 'admin_view' ) );
+        $page_title = DanceParty::NAME . ' Options';
+        $menu_title = DanceParty::NAME;
+        $capability = 'manage_options';
+        $menu_slug = 'danceparty-plugin';
+        $function = array( 'DancePartyAdmin', 'admin_view' );
+        add_menu_page( $page_title, $menu_title, $capability,
+                       $menu_slug, $function);
+    }
+
+    static function register_settings() {
+        $option_group = DanceParty::OPTION_GROUP;
+        $option_name = DanceParty::OPTION_GROUP . '-options';
+        register_setting( $option_group, $option_name );
+
+        $db_section = DanceParty::OPTION_GROUP . '-db';
+        add_settings_section(
+            $db_section,
+            'Database Options',
+            array( 'DancePartyAdmin', 'db_section' ),
+            $option_group
+        );
+
+        add_settings_field(
+            'db_host',
+            'Host',
+            array( 'DancePartyAdmin', 'db_host_field_html' ),
+            $option_group,
+            $db_section
+        );
+    }
+
+    static function db_section() {}
+    static function db_host_field_html() {
+        echo '<input id="db_host" name="db_host" type="text"/>';
     }
 
     static function admin_view() {

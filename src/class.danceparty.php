0c4a06786127d7e44e1045c1e51b294cca7559ce
Alexander Eckert <alexandereckert@hotmail.de>
2018-02-13 18:35:59 -0900

    merge

--- src/class.danceparty.php
+++ src/class.danceparty.php
@@ -31,7 +31,7 @@ class DanceParty
       Router::register_routes();
       flush_rewrite_rules();
 
-      self::create_tables();
+      //self::create_tables();
     }
 
     public static function deactivation_hook() {

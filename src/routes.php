69608abdc77740be28fbecf8da12f889f51a29bf
jjacobson <jjjacobson2@alaska.edu>
2018-02-18 20:41:45 -0800

    Added basic admin create member page

--- src/routes.php
+++ src/routes.php
@@ -8,6 +8,7 @@
 
 Router::route( 'test', 'test.php' );
 Router::route( 'signup', 'signup.php' );
+Router::route( 'create_member', 'create_member.php' );
 Router::route( 'user_model_test', 'umt.php' );
 Router::route( 'show_members', 'show_members.php' );
 Router::route( 'Cameron_test', 'Cameron_test.php' );

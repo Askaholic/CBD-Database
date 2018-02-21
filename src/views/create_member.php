a0fa419e721982cbcbffd47c72a2394f2a99f37d
jjacobson <jjjacobson2@alaska.edu>
2018-02-20 19:16:01 -0800

    clean up create member page

--- src/views/create_member.php
+++ src/views/create_member.php
@@ -6,14 +6,43 @@
 <body>
 <h1 style="margin-left: 10px;">Create a new Contra Borealis Dance account</h1>
 
+<script>
+//can put other limits on password here
+  function check()
+  {
+    if(checkInputs())
+    {
+      document.getElementById("submit").disabled = false;
+      document.getElementById("bad_input").innerHTML = "";
+    }
+    else
+    {
+      document.getElementById("submit").disabled = false;
+    }
+  }
+
+  function checkInputs()
+  {
+    if(!document.getElementById("first").value || !document.getElementById("last").value
+        || !document.getElementById("email").value || !document.getElementById("expiry").value)
+        return false; // empty input field
+    return true;
+  }
+</script>
+
 <form method="post" action="create_member">
   <?php wp_nonce_field('submit', 'create_member_nonce'); ?>
-  <input type="text" name="user"
-   pattern="[A-Za-z]{2,128} [A-Za-z]{2,128}" title="John Doe"
-   placeholder="Enter First and Last Name" style="width: 300px; margin-left: 10px;">
-  <input type="email" name="email"
+  <input type="text" name="first" id="first" onkeyup="check();"
+    pattern="[A-Za-z]{2,128}" title="First name"
+    placeholder="Enter First Name" style="width: 300px; margin-left: 10px;">
+  <input type="text" name="last" id="last" onkeyup="check();"
+    pattern="[A-Za-z]{2,128}" title="Last name"
+    placeholder="Enter Last Name" style="width: 300px; margin-left: 10px;">
+  <input type="email" name="email" id="email" onkeyup="check();"
+    pattern=".*[.]{1}[a-z]{2,4}" title="user@domain.com"
     placeholder="Enter Email Address" style="width: 300px; margin-left: 10px;">
-  <input type="date" name="expiry" style="width: 300px; margin-left: 10px;">
+  <input type="date" name="expiry" id="expiry" onkeyup="check();"
+    style="width: 300px; margin-left: 10px;">
   <input type="submit" id="submit" value="Create Member" style="margin-left: 10px;">
 </form>
 

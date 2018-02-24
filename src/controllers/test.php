d221aef73e8011d7f15bee19959462316ecc1b2f
Aisha Peters <ampeters@alaska.edu>
2018-02-23 16:29:08 -0900

    Readded original test view code.

--- src/controllers/test.php
+++ src/controllers/test.php
@@ -12,6 +12,17 @@
   <hr>
   <h3>Custom Menu</h3>
 <!-- //custom hooks example -->
+<?php do_action('wp_iframe');
+$menu = "index.php/home";
+if (isset($_POST['menu']) && !empty($_POST['menu'])) {
+    $check = $_POST['menu'];
+    if(!empty($check))
+      $menu=$check;
+}
+echo "<iframe src='$menu' width='100%' height='50%'></iframe>";
+?>
+
+<h3>Session Test</h3>
 
 <?php
 require_once( DP_PLUGIN_DIR . 'class.authenticate.php' );
@@ -26,7 +37,6 @@ require_once( DP_PLUGIN_DIR . 'class.authenticate.php' );
 		echo 'Session id: ' . $GLOBALS['session']->get( 'id' );
 
 ?>
-
 <hr>
   <h3>Footer</h3>
   <?php wp_footer(); ?>

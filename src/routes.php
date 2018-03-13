b6a32ba9a5ae66b50917c5dc00bc673031bbc6b8
Askaholic <reweeden@alaska.edu>
2018-03-12 17:44:06 -0800

    Use new route method

--- src/routes.php
+++ src/routes.php
@@ -6,15 +6,12 @@
  *     Router::route( $endpoint, $controller );
  */
 
-Router::route( 'test', 'test.php' );
 Router::route_post( 'Test' , 'test', 'test.php' );
+Router::route_post( 'Sign Up', 'signup', 'signup.php' );
+Router::route_post( 'Create Member', 'create_member', 'create_member.php' );
+Router::route_post( 'Show Members', 'show_members', 'show_members.php' );
+Router::route_post( 'Show Expired Members', 'show_expired_members', 'show_expired_users.php' );
+Router::route_post( 'Create Event', 'create_event', 'create_event.php' );
+Router::route_post( 'Login', 'login', 'login.php' );
 
-Router::route( 'signup', 'signup.php' );
-Router::route( 'create_member', 'create_member.php' );
-Router::route( 'user_model_test', 'umt.php' );
-Router::route( 'show_members', 'show_members.php' );
-Router::route( 'show_expired_members', 'show_expired_users.php' );
-Router::route( 'Cameron_test', 'Cameron_test.php' );
-Router::route( 'create_event', 'create_event.php' );
-Router::route( 'login', 'login.php' );
 ?>

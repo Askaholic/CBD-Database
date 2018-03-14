93f77047bc06910c7f19b1be06214829e386105a
Dope Beats <reweeden@alaska.edu>
2018-03-13 22:39:16 -0800

    Removed html boilerplate since that gets taken care of by wordpress now

--- src/views/layout.php
+++ src/views/layout.php
@@ -1,28 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="<?php echo DanceParty::ASSET_URL . 'fallback.css' ?>">
-        <title><?php echo $title; ?></title>
-        <?php wp_head(); ?>
-    </head>
+<link rel="stylesheet" href="<?php echo DanceParty::ASSET_URL . 'fallback.css' ?>">
 
-    <body>
-        <div class="wrap">
-            <?php if ( isset( $error ) ) { ?>
-                <div class="error">
-                    <p><?php echo $error ?></p>
-                </div>
-            <?php
-            } 
-            if ( isset( $info ) ) { ?>
-                <div class="info">
-                    <p><?php echo $info ?></p>
-                </div>
-            <?php
-            }
-            DanceParty::render_view( $view, $context ); ?>
+<div class="wrap">
+    <?php if ( isset( $error ) && $error !== '' ) { ?>
+        <div class="error">
+            <p><?php echo $error ?></p>
         </div>
-        <?php wp_footer(); ?>
-    </body>
-</html>
+    <?php
+    }
+    if ( isset( $info ) && $info !== '') { ?>
+        <div class="info">
+            <p><?php echo $info ?></p>
+        </div>
+    <?php
+    }
+    DanceParty::render_view( $view, $context ); ?>
+</div>

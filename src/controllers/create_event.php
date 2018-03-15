37b4cf47639dd73e202469098944330c1ec3abea
Askaholic <reweeden@alaska.edu>
2018-03-14 18:03:17 -0800

    Started figuring out the types to use in event table.

--- src/controllers/create_event.php
+++ src/controllers/create_event.php
@@ -2,31 +2,50 @@
 
 require_once( DP_PLUGIN_DIR . 'helpers.php' );
 
-$sql_types = array(
+$schema_types = array(
     'text' => 'text',
     'number' => 'int',
-    'radio' => '',
+    'radio' => 'multivalued',
     'checkbox' => '',
-    'select' => '',
+    'select' => 'multivalued',
 );
 
 $error;
 $info;
 
 if ( isset($_POST['event_schema']) ) {
+
+    require_once( DP_PLUGIN_DIR . 'models/event.php' );
+
     try {
         $field_info = json_decode(stripslashes($_POST['event_schema']), true);
 
         $event_name = not_empty(reg_chars($field_info['name']));
+
         $schema = array(
-            'columns' => array_map( function($value) {
+            'columns' => array_map( function($value) use( &$schema_types ) {
                     $col_name = not_empty(clean_name($value['name']));
                     $col_type = not_empty(clean_name($value['type']));
+                    $col_desc = $value['desc'];
+                    if ( !is_string($col_desc) ) {
+                        $col_desc = '';
+                    }
+                    $col_desc = htmlspecialchars($col_desc);
 
-                    return "$col_name $col_type";
+                    // TODO: Handle checkbox
+                    $col_type = $schema_types[$col_type];
+
+                    return array(
+                        'name' => $col_name,
+                        'type' => $col_type,
+                        'description' => $col_desc,
+                        'constraints' => ''
+                    );
                 }, $field_info['fields'] )
         );
 
+        // TODO: Make this an empty schema. This is still just copy and pasted
+        // from above, so of course it's always going to be equal...
         $schema1 = array(
             'columns' => array_map( function($value) {
                     $col_name = not_empty(clean_name($value['name']));
@@ -36,7 +55,6 @@ if ( isset($_POST['event_schema']) ) {
                 }, $field_info['fields'] )
         );
 
-        require_once( DP_PLUGIN_DIR . 'models/event.php' );
 
         $event = new Event(array(
             'name' => $event_name,
@@ -45,10 +63,10 @@ if ( isset($_POST['event_schema']) ) {
             'schema_info' => json_encode($schema)
         ));
 
-        if ($schema === $schema1) {
-            throw new Exception( 'Cannot insert empty event' );
-        }
-        
+        // if ($schema === $schema1) {
+        //     throw new Exception( 'Cannot insert empty event' );
+        // }
+
         $event->commit();
     }
     catch (PDOException $e) {
@@ -58,7 +76,6 @@ if ( isset($_POST['event_schema']) ) {
     catch (Exception $e) {
         $error = $e->getMessage();
     }
-    print_r($schema);
 }
 
 

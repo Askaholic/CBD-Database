48a9109baa17b704e3e282f5d19aa513b1ca2ff0
sambolias <serie@alaska.edu>
2018-03-17 20:10:06 -0800

    added existing email query

--- src/views/signup_view.php
+++ src/views/signup_view.php
@@ -5,7 +5,7 @@
     wp_nonce_field('submit', 'signup_nonce');
     FormBuilder::input('text', 'first_name', 'First Name', 'pattern="[A-Za-z]{2,128}" required title="First name, letters only"');
     FormBuilder::input('text', 'last_name', 'Last Name', 'pattern="[A-Za-z]{2,128}" required title="Last Name, letters only"');
-    FormBuilder::input('email', 'email', 'Email Address', 'onchange="checkEmailAvailable()" pattern=".*[.]{1}[A-Za-z]{2,4}" required title="Enter valid email address"');
+    FormBuilder::input('email', 'email', 'Email Address', 'pattern=".*[.]{1}[A-Za-z]{2,4}" required title="Enter valid email address"');
     FormBuilder::input('password', 'password', 'Password', 'onkeyup="checkPasswordsMatch()" pattern=".{6,100}" required title="Password must be between 6-100 characters"');
     FormBuilder::input('password', 'confirm_password', 'Confirm Password', 'onkeyup="checkPasswordsMatch()" pattern=".{6,100}" required ');
 ?>

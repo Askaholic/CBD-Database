86a91be8f84d9d6e536293f3d2f6849cbed30aa5
Askaholic <Askaholic907@gmail.com>
2018-04-09 15:30:23 -0800

    Removed debug code.

--- src/views/show_event.php
+++ src/views/show_event.php
@@ -1,11 +1,3 @@
-<?php
-
-//debug
-// echo "event found! <br>";
-// $s = count( $events );
-// $json = $events[0]->schema_info;
-// echo "$json <br><br>";
-?>
 <form method="post" action="">
 <?php
 wp_nonce_field('submit', 'event_nonce');
@@ -18,6 +10,3 @@ DanceParty::render_view('event_snippet.php',
 <!-- Should button say Join Event or something less generic than Submit? -->
 <input type="submit" name="submit" value="Submit">
 </form>
-<?php
-
- ?>

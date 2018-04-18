3f7675e9e0464c0199f3bf4fe2f42516bd6dc3f5
Alexander Eckert <alexandereckert@hotmail.de>
2018-04-17 17:50:17 -0800

    rename show members and show expired members pages

---
+++ src/controllers/inactive_members.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once( DP_PLUGIN_DIR . 'class.authenticate.php' );
+require_once( DP_PLUGIN_DIR . 'models/user.php' );
+require_once( DP_PLUGIN_DIR . 'helpers.php' );
+
+if ( ! Authenticate::is_logged_in() ) {
+    wp_redirect('login/?afterlog=inactive_members');
+}
+
+if ( ! Authenticate::is_admin() ) {
+    die( 'Unauthorized' );
+}
+
+if ( isset( $_POST['renew_member_nonce'] ) && !wp_verify_nonce( $_POST['renew_member_nonce'], 'submit' ) ) {
+    die( 'Bad token' );
+}
+
+$error;
+$info;
+
+if ( isset( $_POST['renew_member_nonce'] ) ) {
+    try {
+        $id = valid_id(not_empty($_POST['id']));
+        $expiry = valid_date(not_empty($_POST['expiry']));
+
+        // Check that the user actually exists
+        try {
+            $usr = new User(
+                array(
+                    'id' => $id
+                )
+            );
+            $usr->pull();
+        } catch ( Exception $e ) {
+            throw new BadInputException("User with id $id does not exist");
+        }
+
+        $memberData = array(
+            'user_id' => $id,
+            'expiration_date' => $expiry
+        );
+        $member = new Membership( $memberData );
+        $member->commit();
+
+    } catch ( Exception $e ) {
+        if ( get_class( $e ) !== BadInputException ) {
+            error_log($e);
+        }
+
+        $error = $e->getMessage();
+
+        if ( get_class($e) === PDOException) {
+            $error = "Database error";
+        }
+    }
+}
+
+
+$members = User::query_all_without_membership();
+
+DanceParty::render_view_with_template( 'inactive_members.php',
+    array(
+        'members' => $members,
+        'error' => $error,
+        'info' => $info
+    )
+);
+
+?>
